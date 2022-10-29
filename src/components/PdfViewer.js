a369a5f23c83ef33d8e2a09d89afefd98ab767e0
Mateusz Tatko <60148744+mtatko@users.noreply.github.com>
2022-10-30 00:37:58 +0200

    refactor: :art: Prettier run and version bump
    Signed-off-by: Mateusz Tatko <60148744+mtatko@users.noreply.github.com>

--- src/components/PdfViewer.js
+++ src/components/PdfViewer.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
-import * as pdfjsLib from "pdfjs-dist/build/pdf";
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
+import React, {useEffect, useState, useRef, useCallback} from 'react';
+import * as pdfjsLib from 'pdfjs-dist/build/pdf';
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
 
 export const PdfViewer = props => {
   const canvasRef = useRef();
@@ -9,19 +9,23 @@ export const PdfViewer = props => {
   const [pdfRef, setPdfRef] = useState();
   const [currentPage, setCurrentPage] = useState(1);
 
-  const renderPage = useCallback((pageNum, pdf=pdfRef) => {
-    pdf && pdf.getPage(pageNum).then(function(page) {
-      const viewport = page.getViewport({scale: 1.5});
-      const canvas = canvasRef.current;
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-      const renderContext = {
-        canvasContext: canvas.getContext('2d'),
-        viewport: viewport
-      };
-      page.render(renderContext);
-    });   
-  }, [pdfRef]);
+  const renderPage = useCallback(
+    (pageNum, pdf = pdfRef) => {
+      pdf &&
+        pdf.getPage(pageNum).then(function (page) {
+          const viewport = page.getViewport({scale: 1.5});
+          const canvas = canvasRef.current;
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;
+          const renderContext = {
+            canvasContext: canvas.getContext('2d'),
+            viewport: viewport,
+          };
+          page.render(renderContext);
+        });
+    },
+    [pdfRef],
+  );
 
   useEffect(() => {
     renderPage(currentPage, pdfRef);
@@ -29,19 +33,19 @@ export const PdfViewer = props => {
 
   useEffect(() => {
     const loadingTask = pdfjsLib.getDocument(props.url);
-    loadingTask.promise.then(loadedPdf => {
-      setPdfRef(loadedPdf);
-    }, function (reason) {
-      console.error(reason);
-    });
+    loadingTask.promise.then(
+      loadedPdf => {
+        setPdfRef(loadedPdf);
+      },
+      function (reason) {
+        console.error(reason);
+      },
+    );
   }, [props.url]);
-
-  const nextPage = () => pdfRef && currentPage < pdfRef.numPages && setCurrentPage(currentPage + 1);
-
-  const prevPage = () => currentPage > 1 && setCurrentPage(currentPage - 1);
 
   const canvasStyle = {
     maxHeight: '100%',
   };
+
   return <canvas style={canvasStyle} ref={canvasRef}></canvas>;
 };
